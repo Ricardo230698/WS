46e6484071814d728a58f7738a96c780969e776e
Ricardo230698 <oticse@hotmail.com>
2023-12-22 20:37:11 -0500

    Lesson 2 team with stretch

--- Lesson2/app.js
+++ Lesson2/app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// Body-parser es una librería de Node. js que se utiliza con Express para analizar y procesar los datos de solicitudes HTTP, como JSON o datos de formulario. Permite acceder a los datos del cuerpo de la solicitud en un formato fácilmente utilizable en una aplicación Node. js.
+// Body-parser es una librería de Node. js que se utiliza con Express para analizar y procesar los datos de solicitudes HTTP, como JSON o datos de formulario. Permite acceder a los datos del cuerpo de la solicitud en un formato fácilmente utilizable en una aplicación Node.js.
 const professionalRoutes = require('./routes/routes');
+const port = process.env.PORT || 8080;
 const app = express();
+
+// Importing the database connection
+const mongodb = require('./DBconnection/mongodb');
 
 // Middlewares
 app.use(bodyParser.json());
@@ -14,4 +18,13 @@ app.use((req, res, next) => {
 
 app.use('/professional', professionalRoutes);
 
-app.listen(8080);
+// Here I'm using the function we created and exported from our connection database file.
+// So far the only difference is that I'm using app.listen inside of this function.
+mongodb.initDb((err, mongodb ) => {
+    if (err) {
+      console.log(err);
+    } else {
+      app.listen(port);
+      console.log('Running');
+    }
+});
