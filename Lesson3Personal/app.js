12336e632d72e5d70a0fc56dd6217bb1b9447c48
Ricardo230698 <oticse@hotmail.com>
2023-12-29 00:50:31 -0500

    Lesson 3 Personal Assignment

---
+++ Lesson3Personal/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const router = require('./routers/index');
+const port = process.env.PORT || 8080;
+const app = express();
+
+// Importing the database connection
+const mongodb = require('./ConnectionDB/mongodb');
+
+// Middlewares
+app.use(bodyParser.json())
+app.use('/', router);
+
+// Here I'm using the function we created and exported from our connection database file.
+// So far the only difference is that I'm using app.listen inside of this function.
+mongodb.initDb((err, mongodb) => {
+    if (err) {
+        console.log(err);
+    } else {
+        app.listen(port);
+        console.log('Running');
+    }
+})
