b337fa1a902d4324d5dd628362683d8291c92f9c
Ricardo230698 <oticse@hotmail.com>
2024-04-24 07:12:53 -0500

    Adding pagination

--- ZOHO CRM/zohocrm/api/controllers/GetEmailsFromViewController.js
+++ ZOHO CRM/zohocrm/api/controllers/GetEmailsFromViewController.js
@@ -8,6 +8,8 @@
 const ZohoAPIService = require("../services/ZohoAPIService");
 const axios = require('axios');
 
+
+
 module.exports = {
   async getCustomViewById(req, res) {
     // Get the needed parameters to construct the request URL
@@ -15,82 +17,189 @@ module.exports = {
     const moduleApiName = req.params.module;
     const authToken = req.headers.authorization;
     const per_page = req.query.per_page;
-    const page_token = req.query.page_token;
-    const page = req.query.page;
+    let page_token;
+    let page;
 
     // Construct the request URL
     // `https://www.zohoapis.com/crm/v6/${moduleApiName}?cvid=${viewId}&fields=Email`
     const baseURL = 'https://www.zohoapis.com/crm/v6';
     let requestUrl = `${baseURL}/${moduleApiName}?cvid=${viewId}&fields=Email`;
-    if (page) {
-      requestUrl += `&page=${page}`;
-    }
-    if (per_page) {
-      requestUrl += `&per_page=${per_page}`;
-    }
-    if (page_token) {
-      requestUrl += `&page_token=${page_token}`;
-    }
+    // if (page) {
+    //   requestUrl += `&page=${page}`;
+    // }
+    // if (per_page) {
+    //   requestUrl += `&per_page=${per_page}`;
+    // }
+    // if (page_token) {
+    //   requestUrl += `&page_token=${page_token}`;
+    // }
 
-    console.log(viewId, moduleApiName, authToken);
-    console.log(requestUrl);
-
-    // Execute the function to get the records from the custom view
-    try {
-      const view = await ZohoAPIService.getView(requestUrl, {
-        headers: {
-          'Authorization': `Zoho-oauthtoken ${authToken}`
-        }
-      });
-
-      console.log(view.data.data);
-      console.log(view.data.info);
-
-      // Save the records from the custom view to an array (but only their IDs)
-      let ids = [];
-      view.data.data.forEach(contact => {
-          ids.push(contact.id);
-      });
-      console.log(ids);
-
-      // Now, unblock emails for the retrieved IDs
+    for (let index = 0; index < 5; index++) {      
+      // Execute the function to get the records from the custom view
       try {
-        const zohoResponse = await axios.post("https://www.zohoapis.com/crm/v6/Contacts/actions/unblock_email", {
-              "unblock_fields": [
-                  "Email",
-              ],
-              "ids": ids
-            }, {
-              "headers": {
-                Authorization: `Zoho-oauthtoken ${authToken}`
-              }
-            }
-        )
-        // Handle Zoho CRM API response
-        if (zohoResponse.status === 200) {
-          return res.ok({ message: 'Emails unblocked successfully' });
-        } else if (zohoResponse.status === 400) {
-        // Handle specific error cases
-        // You may need to parse Zoho's error response for detailed error messages
-        // For simplicity, I'm returning a generic error message
-          return res.badRequest({ error: 'Failed to unblock emails. Please check your request.' });
-        } else if (zohoResponse.status === 401) {
-            return res.unauthorized({ error: 'Unauthorized. Authentication failed.' });
-        } else if (zohoResponse.status === 404) {
-            return res.notFound({ error: 'Zoho API endpoint not found.' });
-        } else if (zohoResponse.status === 500) {
-            return res.serverError({ error: 'Internal Server Error. Please contact support.' });
-        } else {
-            console.log(zohoResponse.data.data);
-            return res.serverError({ error: 'Unexpected error occurred.' });
+        requestUrl = `${baseURL}/${moduleApiName}?cvid=${viewId}&fields=Email`;
+        if (page) {
+          requestUrl += `&page=${page}`;
         }
+        if (per_page) {
+          requestUrl += `&per_page=${per_page}`;
+        }
+        if (page_token) {
+          requestUrl += `&page_token=${page_token}`;
+        }
+        console.log(viewId, moduleApiName, authToken);
+        console.log(requestUrl);
+        const view = await ZohoAPIService.getView(requestUrl, {
+          headers: {
+            'Authorization': `Zoho-oauthtoken ${authToken}`
+          }
+        });
+    
+        console.log(view.data.data);
+        console.log(view.data.info);
+    
+        // Save the records from the custom view to an array (but only their IDs)
+        let ids = [];
+        view.data.data.forEach(contact => {
+            ids.push(contact.id);
+        });
+        console.log(ids);
+
+        page = null;
+        page_token = view.data.info.next_page_token;
+        console.log(page_token)
+    
+        // // Now, unblock emails for the retrieved IDs
+        // try {
+        //   const zohoResponse = await axios.post("https://www.zohoapis.com/crm/v6/Contacts/actions/unblock_email", {
+        //         "unblock_fields": [
+        //             "Email",
+        //         ],
+        //         "ids": ids
+        //       }, {
+        //         "headers": {
+        //           Authorization: `Zoho-oauthtoken ${authToken}`
+        //         }
+        //       }
+        //   )
+        //   // Handle Zoho CRM API response
+        //   if (zohoResponse.status === 200) {
+        //     return res.ok({ message: 'Emails unblocked successfully' });
+        //   } else if (zohoResponse.status === 400) {
+        //   // Handle specific error cases
+        //   // You may need to parse Zoho's error response for detailed error messages
+        //   // For simplicity, I'm returning a generic error message
+        //     return res.badRequest({ error: 'Failed to unblock emails. Please check your request.' });
+        //   } else if (zohoResponse.status === 401) {
+        //       return res.unauthorized({ error: 'Unauthorized. Authentication failed.' });
+        //   } else if (zohoResponse.status === 404) {
+        //       return res.notFound({ error: 'Zoho API endpoint not found.' });
+        //   } else if (zohoResponse.status === 500) {
+        //       return res.serverError({ error: 'Internal Server Error. Please contact support.' });
+        //   } else {
+        //       console.log(zohoResponse.data.data);
+        //       return res.serverError({ error: 'Unexpected error occurred.' });
+        //   }
+        // } catch (error) {
+        //   // Handle unexpected errors
+        //   sails.log.error('Error occurred:', error.response.data);
+        //   return res.serverError({ error: 'An unexpected error occurred.' });          
+        // }
       } catch (error) {
-        // Handle unexpected errors
-        sails.log.error('Error occurred:', error.response.data);
-        return res.serverError({ error: 'An unexpected error occurred.' });          
-      }
-    } catch (error) {
-      console.error('Error fetching custom view by ID: ', error.response);
+        // console.error('Error fetching custom view by ID: ', error.response);
+        console.error('Error fetching custom view by ID: ', error.response);
+        return res.serverError({ error: 'An unexpected error occurred.' });
+      } 
     }
+    // Aquí termina el ciclo FOR
+    
   }
 };
+
+
+// module.exports = {
+//   async getCustomViewById(req, res) {
+//     // Get the needed parameters to construct the request URL
+//     const viewId = req.params.viewId;
+//     const moduleApiName = req.params.module;
+//     const authToken = req.headers.authorization;
+//     const per_page = req.query.per_page;
+//     const page_token = req.query.page_token;
+//     const page = req.query.page;
+
+//     // Construct the request URL
+//     // `https://www.zohoapis.com/crm/v6/${moduleApiName}?cvid=${viewId}&fields=Email`
+//     const baseURL = 'https://www.zohoapis.com/crm/v6';
+//     let requestUrl = `${baseURL}/${moduleApiName}?cvid=${viewId}&fields=Email`;
+//     if (page) {
+//       requestUrl += `&page=${page}`;
+//     }
+//     if (per_page) {
+//       requestUrl += `&per_page=${per_page}`;
+//     }
+//     if (page_token) {
+//       requestUrl += `&page_token=${page_token}`;
+//     }
+
+//     console.log(viewId, moduleApiName, authToken);
+//     console.log(requestUrl);
+
+//     // Execute the function to get the records from the custom view
+//     try {
+//       const view = await ZohoAPIService.getView(requestUrl, {
+//         headers: {
+//           'Authorization': `Zoho-oauthtoken ${authToken}`
+//         }
+//       });
+
+//       console.log(view.data.data);
+//       console.log(view.data.info);
+
+//       // Save the records from the custom view to an array (but only their IDs)
+//       let ids = [];
+//       view.data.data.forEach(contact => {
+//           ids.push(contact.id);
+//       });
+//       console.log(ids);
+
+//       // Now, unblock emails for the retrieved IDs
+//       try {
+//         const zohoResponse = await axios.post("https://www.zohoapis.com/crm/v6/Contacts/actions/unblock_email", {
+//               "unblock_fields": [
+//                   "Email",
+//               ],
+//               "ids": ids
+//             }, {
+//               "headers": {
+//                 Authorization: `Zoho-oauthtoken ${authToken}`
+//               }
+//             }
+//         )
+//         // Handle Zoho CRM API response
+//         if (zohoResponse.status === 200) {
+//           return res.ok({ message: 'Emails unblocked successfully' });
+//         } else if (zohoResponse.status === 400) {
+//         // Handle specific error cases
+//         // You may need to parse Zoho's error response for detailed error messages
+//         // For simplicity, I'm returning a generic error message
+//           return res.badRequest({ error: 'Failed to unblock emails. Please check your request.' });
+//         } else if (zohoResponse.status === 401) {
+//             return res.unauthorized({ error: 'Unauthorized. Authentication failed.' });
+//         } else if (zohoResponse.status === 404) {
+//             return res.notFound({ error: 'Zoho API endpoint not found.' });
+//         } else if (zohoResponse.status === 500) {
+//             return res.serverError({ error: 'Internal Server Error. Please contact support.' });
+//         } else {
+//             console.log(zohoResponse.data.data);
+//             return res.serverError({ error: 'Unexpected error occurred.' });
+//         }
+//       } catch (error) {
+//         // Handle unexpected errors
+//         sails.log.error('Error occurred:', error.response.data);
+//         return res.serverError({ error: 'An unexpected error occurred.' });          
+//       }
+//     } catch (error) {
+//       console.error('Error fetching custom view by ID: ', error.response);
+//     }
+//   }
+// };
